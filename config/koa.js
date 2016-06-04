801f52bed297fbd814f0cde0a71d3c3eac5246e9
wodog <536505032@qq.com>
2016-06-04 14:00:34 +0800

    add cors

--- config/koa.js
+++ config/koa.js
@@ -4,12 +4,14 @@ const logger = require("koa-logger");
 const compress = require("koa-compress");
 const bodyParser = require("koa-bodyparser");
 const errorHandler = require('../src/middlewares/errorHandler');
+const cors = require('koa-cors');
 
 
 module.exports = function(app, config) {
   if (config.app.env !== "test") {
     app.use(logger());
   }
+  app.use(cors());
   app.use(bodyParser());
   app.use(compress());
   app.use(errorHandler());
